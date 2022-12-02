c6f9809f10955c47b9d9fa0db6a0c05552c6e561
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-12-01 22:21:56 -0300

    Add config redux-toolkit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./ui/homePage/HomePage.jsx";
+import ProfilePage from "./ui/profilePage/ProfilePage.jsx";
+import LoginPage from "./ui/loginPage/LoginPage.jsx";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme.js";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
