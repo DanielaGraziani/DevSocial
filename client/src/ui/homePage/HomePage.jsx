30bb3aec0f70a050cd3cad0ada00017c0dda1502
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-12-01 22:19:34 -0300

    Add principal components

---
+++ client/src/ui/homePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box} from '@mui/material'
+import Navbar from "../../ui/navbar/Navbar.jsx"
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar/>
+    </Box>
+  )
+}
+
+export default HomePage
